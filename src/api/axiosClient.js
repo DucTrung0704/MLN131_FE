13260e305d875b492732938b6af2a400305b1e0b
DucTrung0704 <trungzero4869@gmail.com>
2025-10-07 00:22:25 +0700

    update v2

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
     headers: {
         "Content-Type": "application/json",
     },
@@ -13,6 +13,5 @@ axiosClient.interceptors.request.use((config) => {
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
-
 
 export default axiosClient;
